200a815450bfe87f8f418d07c0a497fc1320e57c
Sumit Ukey <ukeysumit123@gmail.com>
2022-02-24 22:48:21 +0530

    Rename method name

---
+++ SumOfDigitsProgram.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class SumOfDigitsProgram {
+
+   public static int SumOfNumbers(int number) {
+      // declare variables
+      int lastDigit = 0;
+      int sum = 0;
+
+      // loop to repeat the process
+      while(number != 0) {
+
+         // find last digit
+         lastDigit = number % 10;
+
+         // add last digit to sum
+         sum = sum + lastDigit;
+
+         // remove last digit
+         number = number / 10;
+      }
+
+      // sum value is the sum of digits
+      // of the given number
+      return sum;
+  }
+
+  public static void main(String[] args) {
+      // declare variables
+      int number = 0;
+      int sumOfDigits = 0;
+
+      // create Scanner class object 
+      // for reading the values
+      Scanner scan =  new Scanner(System.in);
+
+      // read input
+      System.out.print("Enter an integer number::");
+      number = scan.nextInt();
+
+      // find sum of digits of number
+      sumOfDigits = SumOfNumbers(number);
+
+      // display result
+      System.out.println("The sum of digits of" +
+         " the number "+number+" = "+sumOfDigits);
+
+      // close Scanner class object
+      scan.close();
+  }
+}
